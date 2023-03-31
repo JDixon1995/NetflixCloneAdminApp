2ccb0ac959f41015d9e35c386c39c9670d8b943e
John <jddhalogamer@gmail.com>
2023-03-31 08:00:45 -0500

    moved data call to Home page from App component.

--- src/App.js
+++ src/App.js
@@ -9,33 +9,8 @@ import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useEffect, useState, useMemo } from 'react'
-import axios from 'axios'
  
 function App() {
-
-  const MONTHS = useMemo( () => [
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-  ], [])
-
-  const [userStats, setUserStats] = useState([])
-    
-  useEffect(() => {
-    const getStats = async () => {
-      try {
-        const res = await axios.get('/users/stats', {
-          headers: {
-            token : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MThlMzJjM2RlYzI2M2IwNzMwZjU4ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDIxMzQwNiwiZXhwIjoxNjgwNjQ1NDA2fQ.j5LZ0EiRbp2nKdJF918IQVjTF07a4DKHEF0S6FlJkzs"
-          }
-        })
-        res.data.map(item => setUserStats(prev => [...prev, {name: MONTHS[item._id - 1], 'New User' : item.total }]))
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    getStats()
-  }, [MONTHS])
-  console.log(userStats)
 
   return (
     <Router>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,8 +4,34 @@ import "./home.css";
 import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
+import { useEffect, useState, useMemo } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+
+  const MONTHS = useMemo( () => [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+  ], [])
+
+  const [userStats, setUserStats] = useState([])
+    
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axios.get('/users/stats', {
+          headers: {
+            token : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MThlMzJjM2RlYzI2M2IwNzMwZjU4ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MDIxMzQwNiwiZXhwIjoxNjgwNjQ1NDA2fQ.j5LZ0EiRbp2nKdJF918IQVjTF07a4DKHEF0S6FlJkzs"
+          }
+        })
+        res.data.map(item => setUserStats(prev => [...prev, {name: MONTHS[item._id - 1], 'New User' : item.total }]))
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getStats()
+  }, [MONTHS])
+  console.log(userStats)
+
   return (
     <div className="home">
       <FeaturedInfo />
