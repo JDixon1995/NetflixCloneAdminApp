e33535bb7a37465b0ebeafdc402b0431121969bb
John <jddhalogamer@gmail.com>
2023-04-05 09:10:33 -0500

    added contectAPI to auth.

---
+++ src/context/authContext/AuthReducer.js
@@ -0,0 +1,30 @@
+const AuthReducer = (state, reducer ) => {
+    switch (action.type) {
+        case "LOGIN_START" :
+            return {
+                user: null,
+                isFetching: true,
+                error: false,
+            }
+        case "LOGIN_SUCCESS" :
+            return {
+                user: action.payload,
+                isFetching: false,
+                error: false
+            }
+        case "LOGIN_FAILURE":
+            return {
+                user: null,
+                isFetching: false,
+                error: true
+            }
+        case "LOGOUT":
+            return {
+                user: null,
+                isFetching: false,
+                error: false
+            }
+        default:
+            return {...state}
+    }
+}
